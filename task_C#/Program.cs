35f6ce75b51a1b33fa219605a87677a8357bf525
Ivan <vanya1254@yandex.ru>
2022-11-30 17:29:02 +0300

    add final working program

--- task_C#/Program.cs
+++ task_C#/Program.cs
@@ -3,7 +3,7 @@
     int count = default;
 
     for (int i = 0; i < arrayCount.Length; i++)
-    {
+    {                                                                                       // делает подсчеты подходящих условию элементов массива строк
         if (arrayCount[i].Length <= 3)
         {
             count++;
@@ -18,7 +18,7 @@ string[] ArrayStringShorter4Symbols(string[] arrayIn, int count)
     string[] arrayOut = new string[count];
     int index = default;
 
-    for (int j = 0; j < arrayIn.Length; j++)
+    for (int j = 0; j < arrayIn.Length; j++)                                                // использует предыдущие подсчеты, создает и заполняет массив строк нужного размера
     {
         if (arrayIn[j].Length <= 3) arrayOut[index++] = arrayIn[j];
     }
@@ -28,20 +28,22 @@ string[] ArrayStringShorter4Symbols(string[] arrayIn, int count)
 
 void PrintStringArray(string[] array)
 {
-    Console.Write("[");
-    for (int i = 0; i < array.Length - 1; i++)
+    if (array.Length == 0)
     {
-        Console.Write($"{array[i]}, ");
+        Console.WriteLine("[]");
     }
-    Console.WriteLine($"{array[array.Length - 1]}]");
+    else
+    {
+        Console.Write("[");
+        for (int i = 0; i < array.Length - 1; i++)
+        {                                                                                   // выводит массив строк с проверкой на пустой массив
+            Console.Write($"{array[i]}, ");
+        }
+        Console.WriteLine($"{array[array.Length - 1]}]");
+    }
 }
 
-string[] arrayCheck = {"hello", "2", "world", ":-)"};
+string[] arrayCheck = { "hello", "2", "world", ":-)" };                                     // задаем исходный массив строк
 int counterElems = CounterArrayStringShorter4Symbols(arrayCheck);
 string[] arrayVerified = ArrayStringShorter4Symbols(arrayCheck, counterElems);
-
-if (arrayVerified.Length == 0)
-{
-    Console.WriteLine("[]");
-}
-else PrintStringArray(arrayVerified);
+PrintStringArray(arrayVerified);
